3932bfaac786b4fb056ab0f05eaaac1b65474c8a
NimbusFox <simonbailey10@hotmail.com>
2018-02-03 18:45:08 +0000

    Initial Commit

---
+++ Login-Site/App_Start/SignalR.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+
+namespace Login_Site.App_Start {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            app.MapSignalR();
+        }
+    }
+}
